f93d4e768887e75840cd62fc2a06c635952c1ae1
veronica schiller <veronicaschiller17@gmail.com>
2025-02-06 23:03:27 -0300

    ✅ criação de logica conforme solicitado

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+
+// Declarações ----------
+let AmigosSecretros = [];
+
+// funções --------------
+
+function adicionarAmigo() {
+  let input = document.getElementById("amigo");
+  let nomes = input.value.trim("");
+
+  if (nomes === "") {
+    alert("preencha o campo com o nome do seus amigos");
+    return;
+  }
+  AmigosSecretros.push(nomes);
+  AtualizaListaAmigos();
+}
+
+function AtualizaListaAmigos() {
+  let list = document.getElementById("listaAmigos");
+  list.innerHTML = "";
+
+  for (let i = 0; i < AmigosSecretros.length; i++) {
+    let li = document.createElement("li");
+    li.textContent = AmigosSecretros[i];
+    list.appendChild(li);
+  }
+}
+
+function sortearAmigo() {
+    if(AmigosSecretros.length === 0){
+        alert("Sua lista de amigos esta vazia! Adicione algum amigo")
+        return
+    }
+    let sorteio= Math.floor(Math.random() * AmigosSecretros.length);
+    let amigoSorteado = AmigosSecretros[sorteio];
+
+    document.getElementById("resultado").innerHTML = `Seu amigo é <strong> ${amigoSorteado}<strong>`
+}
